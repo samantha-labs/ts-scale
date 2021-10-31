9e2e800feadfa1fab50fa56cbc82872a1ab44061
Samantha <sam.t.nguyenn@gmail.com>
2021-10-31 01:48:41 -0500

    [src] export constants as well

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { newArithmeticProgression, newGeometricProgression, newHarmonicProgression } from './generators';
 export { isArithmeticProgression, isGeometricProgression, isHarmonicProgression } from './predicates';
 export { ProgressionGenerator, ProgressionPredicate, ProgressionResult, ProgressionType } from './types';
+export { MinorSecond, MajorSecond, MinorThird, MajorThird, PerfectThird, AugumentedFourth, PerfectFifth, GoldenRatio, MajorSixth, MinorSeventh } from './consts';
