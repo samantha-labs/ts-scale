9714c31f4ee969f940ecd72f863b37122fd594b1
Samantha <sam.t.nguyenn@gmail.com>
2021-11-12 11:54:07 -0600

    [cleanup] fix ESLint rule errors in tests
    rules:
     - fix `jest/standalone-expect`
     - fix `@typescript-eslint/semi`

--- tests/totals.spec.ts
+++ tests/totals.spec.ts
@@ -29,7 +29,7 @@ describe('series', () => {
 
 		test('returns empty sum if bounds are equal to each other', () => {
 			expect((getSumFromBounds(5, 5))).toBe(0);
-		})
+		});
 	});
 });
 
@@ -41,7 +41,7 @@ describe('product', () => {
 
 		test('generates correctly with default lambda', () => {
 			expect(getProductFromArray([1, 2, 3, 4, 5])).toBe(120);
-		})
+		});
 
 		test('returns empty product if array is empty', () => {
 			expect(getProductFromArray([])).toBe(1);
@@ -49,13 +49,13 @@ describe('product', () => {
 	});
 
 	describe('from bounds', () => {
-		describe('generates correctly', () => {
+		test('generates correctly', () => {
 			expect(getProductFromBounds(1, 5, (n) => 2 * n)).toBe(3840);
 		});
 
-		describe('generates correctly with default lambda', () => {
+		test('generates correctly with default lambda', () => {
 			expect(getProductFromBounds(1, 5)).toBe(120);
-		})
+		});
 
 		test('throws error if lower bound is greater than upper bound', () => {
 			expect(() => getProductFromBounds(5, 1)).toThrow(RangeError);
@@ -63,6 +63,6 @@ describe('product', () => {
 
 		test('returns empty product if bounds are equal to each other', () => {
 			expect((getProductFromBounds(5, 5))).toBe(1);
-		})
+		});
 	});
 });
